44c01b0f45152ed6727d78036128b98f63ca04aa
shichao13 <shichao.ma13@gmail.com>
2014-03-14 10:24:46 -0400

    3e Should Work

---
+++ 3e Take 2.ml
@@ -0,0 +1,31 @@
+let remove_upward_two (n: pair) (rem: pair option)
+      (left: dict) (right: dict) (dir: direction2) : hole =
+    match dir,n,left,right with
+      | Left2,x,l,Two(m,y,r) -> Hole(rem,Three(l,x,m,y,r))
+      | Right2,y,Two(l,x,m),r -> Hole(rem,Three(l,x,m,y,r))
+      | Left2,x,a,Three(b,y,c,z,d) -> Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
+      | Right2,z,Three(a,x,b,y,c),d -> Absorbed(rem,Two(Two(a,x,b),y,Two(c,z,d)))
+      | Left2,_,_,_ | Right2,_,_,_ -> Absorbed(rem,Two(Leaf,n,Leaf))
+
+  (* Upward phase for removal where the parent of the hole is a Three node.
+   * See cases (3-4) on the handout. n1 and n2 are the (key,value) pairs
+   * contained in the parent node; left, middle, and right are the subtrees
+   * of the parent node (our hole is one of these subtrees); and dir indicates
+   * which subtree was the tree contained by the hole. *)
+  let remove_upward_three (n1: pair) (n2: pair) (rem: pair option)
+      (left: dict) (middle: dict) (right: dict) (dir: direction3) : hole =
+    match dir,n1,n2,left,middle,right with
+      | Left3,x,z,a,Two(b,y,c),d -> Absorbed(rem,Two(Three(a,x,b,y,c),z,d))
+      | Mid3,y,z,Two(a,x,b),c,d -> Absorbed(rem,Two(Three(a,x,b,y,c),z,d))
+      | Mid3,x,y,a,b,Two(c,z,d) -> Absorbed(rem,Two(a,x,Three(b,y,c,z,d)))
+      | Right3,x,z,a,Two(b,y,c),d -> Absorbed(rem,Two(a,x,Three(b,y,c,z,d)))
+      | Left3,w,z,a,Three(b,x,c,y,d),e -> 
+        Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
+      | Mid3,y,z,Three(a,w,b,x,c),d,e -> 
+        Absorbed(rem,Three(Two(a,w,b),x,Two(c,y,d),z,e))
+      | Mid3,w,x,a,b,Three(c,y,d,z,e) -> 
+        Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
+      | Right3,w,z,a,Three(b,x,c,y,d),e -> 
+        Absorbed(rem,Three(a,w,Two(b,x,c),y,Two(d,z,e)))
+      | Left3,_,_,_,_,_ | Mid3,_,_,_,_,_ | Right3,_,_,_,_,_ ->
+        Absorbed(rem,Three(Leaf,n1,Leaf,n2,Leaf))
